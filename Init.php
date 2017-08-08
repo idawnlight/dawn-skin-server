a6aaada757c7ed876f65d3f0af26e96b781b0922
黎明余光 <m1255162259@gmail.com>
2017-08-08 17:36:34 +0800

    Fix path

--- Init.php
+++ Init.php
@@ -5,7 +5,11 @@ use XDO\XDO;
 $db = XDO::Database("System");
 
 $GLOBALS["DSS"]["version"] = "Alpha 0.1.0";
-$GLOBALS["DSS"]["root"] = explode("index.php",$_SERVER['PHP_SELF'])[0];
+if ($GLOBALS['_C']['RouteBase']=="") {
+	$GLOBALS["DSS"]["root"] = "/";
+} else {
+	$GLOBALS["DSS"]["root"] = $GLOBALS['_C']['RouteBase']."/";
+}
 $GLOBALS["DSS"]["title"] = current($db->get("Config.where[name=title]"))["value"];
 $GLOBALS["DSS"]["head"] = current($db->get("Config.where[name=head]"))["value"];
 $GLOBALS["DSS"]["intro"] = current($db->get("Config.where[name=intro]"))["value"];
