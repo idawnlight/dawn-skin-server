014d534b40a395364ac5110dc3464a91b7ec0629
黎明余光 <m1255162259@gmail.com>
2017-08-09 20:11:25 +0800

    Update XDO to 0.2.1

--- index.php
+++ index.php
@@ -1,47 +1,80 @@
 <?php
     /**
-     * XPHP Project
-     * By xtl
-     *
+     * This file is a part of XPHP.
+     * 
+     * You may read the LICENSE file to
+     * know what you can do.
+     * 
+     * @pacakge XPHP
+     * @author xtl<xtl@xtlsoft.top>
+     * @license MIT
+     * 
      */
-
+    
     use X\Autoloader;
     use X\Error;
     use X\Route;
     use X\App;
+    use X\Log;
     use XDO\Tool as XDOTool;
     use XDO\XDO;
-
-    define("X",true);
-    define("XPHP_VERSION","0.1.1-alpha");
-    define("SysDir",dirname(__FILE__).'/');
-    define("LibDir",SysDir.'Lib/');
-    define("AppDir",SysDir.'App/');
-	define("ResDir",SysDir.'Res/');
-    define("TemplateDir",SysDir.'Var/Template/');
-    define("LangDir",SysDir.'Var/Lang/');
-    define("DatDir",SysDir.'Var/Data/');
-    define("RouteDir",SysDir.'Var/Route/');
-
+    
+    /*********************************/ //@@@@@@@@@@@@@@@@@@
+    /*--- Define Path And Version ---*/
+    ##################################
+    define("X",true); //Make sure you are in XPHP
+    define("XPHP_VERSION","0.2.1-stable"); //XPHP Version
+    define("SysDir",dirname(__FILE__).'/'); //XPHP Base Directory
+    define("LibDir",SysDir.'Lib/'); //XPHP Embedded Library Directory
+    define("AppDir",SysDir.'App/'); //XPHP Application Directory
+    define("TemplateDir",SysDir.'Var/Template/'); //XPHP Template Directory
+    define("LangDir",SysDir.'Var/Lang/'); //XPHP Language Directory
+    define("DatDir",SysDir.'Var/Data/'); //XDO Data Directory
+    define("RouteDir",SysDir.'Var/Route/'); //XPHP Route Directory
+    define("ComposerDir",SysDir.'vendor/'); //Composer Vendor Directory
+    ##################################
+          /* --- Define End --- */
+    /*********************************/ //@@@@@@@@@@@@@@@@@@
+    
+    //Require the BASE Config file
     require(SysDir. 'Config.php');
-
-    require(LibDir.'X/Autoloader.php');
-
-    Autoloader::load("X\Error");
-
+    
+    //Require XPHP Embedded Library Autoloader
+    require_once(LibDir.'X/Autoloader.php');
+    
+    //Require Composer Autoloader
+    require_once(ComposerDir.'autoload.php');
+    
+    //Load Logger
+    Autoloader::load("X\\Log");
+    
+    //Load Error Proccesser
+    Autoloader::load("X\\Error");
+    
+    //Init the session (Actions with session could do in Config.php)
     @session_start();
-
+    
+    //Set XDO Dir
     XDO::setDir(DatDir);
-    XDO::$Cache = false; //Disable the cache
-
-    require(SysDir. 'Init.php');
-
+	//Disable XDO Cache
+	XDO::$Cache = false;
+	
+	//Some Dawn Skin Server Settings
+	require(SysDir. 'Init.php');
+    
+    //List the routes
     foreach(XDOTool::listDir(RouteDir,0) as $v){
+        //Load the Route
         Route::load(RouteDir. $v);
     }
-
+    
+    //Run Application
     if(Route::$isLoad){
         App::Run(Route::$Application,Route::$Controller,Route::$Action,Route::$var);
     }else{
         Error::HTTP_E(404);
     }
+    
+      ###############################################
+     /*--------Stop-Reading-Start-Writing!---------*/
+    ################################################
