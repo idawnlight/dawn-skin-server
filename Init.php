197b7e42d1a31dfad60add217242878156aca804
黎明余光 <m1255162259@gmail.com>
2017-08-15 17:00:12 +0800

    An improvement

--- Init.php
+++ Init.php
@@ -7,11 +7,7 @@ use X\Error;
 $db = XDO::Database("System");
 
 $GLOBALS["DSS"]["version"] = "Alpha 0.1.1";
-if ($GLOBALS['_C']['RouteBase']=="") {
-	$GLOBALS["DSS"]["root"] = "/";
-} else {
-	$GLOBALS["DSS"]["root"] = $GLOBALS['_C']['RouteBase']."/";
-}
+$GLOBALS["DSS"]["root"] = $GLOBALS['_C']['RouteBase']."/";
 $GLOBALS["DSS"]["title"] = current($db->get("Config.where[name=title]"))["value"];
 $GLOBALS["DSS"]["head"] = current($db->get("Config.where[name=head]"))["value"];
 if (!is_writeable(DatDir . "Models.json")) {
